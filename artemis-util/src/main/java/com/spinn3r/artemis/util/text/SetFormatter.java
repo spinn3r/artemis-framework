ac492bcfdb5a91be9ae08ba98528e4b3860bedfd
burton <burton@spinn3r.com>
2016-01-10 14:29:50 -0800

    trying to make the forum system work with forums now... seems mostly ready to go...

--- artemis-util/src/main/java/com/spinn3r/artemis/util/text/SetFormatter.java
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/text/SetFormatter.java
@@ -2,12 +2,29 @@ package com.spinn3r.artemis.util.text;
 
 import com.google.common.collect.Sets;
 
+import java.util.Map;
 import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  *
  */
 public class SetFormatter {
+
+    public static  <E> String table(Set<E> set) {
+        checkNotNull( set );
+
+        StringBuilder buff = new StringBuilder();
+
+        for (E e : set) {
+            buff.append( e.toString() );
+            buff.append( "\n" );
+        }
+
+        return buff.toString();
+
+    }
 
     public static String toString( Set<?> set ) {
 
