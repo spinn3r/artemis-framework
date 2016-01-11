3be8918fbf0191b052089d6c84efecf4b9906706
burton <burton@spinn3r.com>
2016-01-10 16:18:43 -0800

    don't throw a runtime exception ... throw a NetworkException...

--- artemis-network/src/main/java/com/spinn3r/artemis/network/init/StandardSocketOptionsConfig.java
+++ artemis-network/src/main/java/com/spinn3r/artemis/network/init/StandardSocketOptionsConfig.java
@@ -4,4 +4,13 @@ package com.spinn3r.artemis.network.init;
  *
  */
 public class StandardSocketOptionsConfig {
+
+    static class SocketOptionProfile {
+
+        private int receiveBufferSize;
+
+        private int sendBufferSize;
+
+    }
+
 }
---
+++ artemis-network/src/test/java/com/spinn3r/artemis/network/init/StandardSocketOptionsServiceTest.java
@@ -0,0 +1,17 @@
+package com.spinn3r.artemis.network.init;
+
+import org.junit.Test;
+
+import java.net.StandardSocketOptions;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class StandardSocketOptionsServiceTest {
+
+    @Test
+    public void testDefaultSocketOptions() throws Exception {
+
+    }
+
+}
