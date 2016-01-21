be38e834298dd7de288d40200854cc6a36906733
burton <burton@spinn3r.com>
2016-01-20 20:32:25 -0800

    Write standard headers about the file format...

---
+++ artemis-byte-block-stream/src/main/java/com/spinn3r/artemis/byte_block_stream/StandardHeaders.java
@@ -0,0 +1,12 @@
+package com.spinn3r.artemis.byte_block_stream;
+
+/**
+ *
+ */
+public class StandardHeaders {
+
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    public static final String CONTENT_CHARSET = "Content-Charset";
+
+}
