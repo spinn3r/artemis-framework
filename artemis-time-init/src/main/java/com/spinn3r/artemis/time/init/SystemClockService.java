8dddcfff22ef7ed995e829c356b7852fb45975d6
burton <burton@spinn3r.com>
2016-01-20 16:58:47 -0800

    force UTC in the system clock.

--- artemis-time-init/src/main/java/com/spinn3r/artemis/time/init/SystemClockService.java
+++ artemis-time-init/src/main/java/com/spinn3r/artemis/time/init/SystemClockService.java
@@ -4,6 +4,9 @@ import com.spinn3r.artemis.init.BaseService;
 import com.spinn3r.artemis.init.Config;
 import com.spinn3r.artemis.time.Clock;
 import com.spinn3r.artemis.time.SystemClock;
+import com.spinn3r.artemis.time.TimeZones;
+
+import java.util.TimeZone;
 
 /**
  *
@@ -12,6 +15,7 @@ public class SystemClockService extends BaseService {
 
     @Override
     public void init() {
+        TimeZone.setDefault( TimeZones.UTC );
         advertise( Clock.class, new SystemClock() );
     }
 
