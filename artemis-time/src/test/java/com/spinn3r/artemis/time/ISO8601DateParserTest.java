32c921634cebb3aaf611f164a6f195e97e5bce9f
burton <burton@spinn3r.com>
2016-02-29 10:04:53 -0800

    better documentation on loading sources, more work on preview/training... ISO 8601 tests.

--- artemis-time/src/test/java/com/spinn3r/artemis/time/ISO8601DateParserTest.java
+++ artemis-time/src/test/java/com/spinn3r/artemis/time/ISO8601DateParserTest.java
@@ -2,6 +2,10 @@ package com.spinn3r.artemis.time;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -17,4 +21,19 @@ public class ISO8601DateParserTest {
         //              ISO8601DateParser.parse( "2014-10-11" ) );
 
     }
+
+
+    @Test
+    public void testParseWithJava8() throws Exception {
+
+//        String strDate = "2015-08-04";
+//        LocalDate aLD = LocalDate.parse(strDate);
+//        System.out.println("Date: " + aLD);
+
+        String strDatewithTime = "2016-02-27 05:28:16 -0500";
+        LocalDateTime aLDT = LocalDateTime.parse(strDatewithTime);
+        System.out.println("Date with Time: " + aLDT);
+
+    }
+
 }
