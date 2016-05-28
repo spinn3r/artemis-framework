4c1723b541263e14cc6b5fafbc90ff077d971596
burton <burton@spinn3r.com>
2016-05-27 17:43:41 -0700

    cleanup --skip-ci

--- artemis-util/src/main/java/com/spinn3r/artemis/util/collections/ImmutableDictionary.java
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/collections/ImmutableDictionary.java
@@ -6,7 +6,6 @@ import com.google.common.collect.ImmutableSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.BiConsumer;
 
 /**
