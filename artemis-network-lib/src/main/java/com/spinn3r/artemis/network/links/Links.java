a182eabb13183f03b68884207ff9a777a5bd2eb0
burton <burton@spinn3r.com>
2016-06-24 13:35:04 -0700

    check for valid links...

--- artemis-network-lib/src/main/java/com/spinn3r/artemis/network/links/Links.java
+++ artemis-network-lib/src/main/java/com/spinn3r/artemis/network/links/Links.java
@@ -14,12 +14,13 @@ import java.util.List;
 public class Links {
 
     /**
-     * Return true if this is a valid and parseable HTTP URL.  It still might
+     * Return true if this is a valid HTTP URL which we can parse.  It still might
      * not be fetchable (perhaps because the URL is down) but at least its valid.
      */
-    public static boolean isHttpURL( String link ) {
+    public static boolean isValid(String link) {
 
         try {
+
             URI uri = new URI( link );
 
             return uri.getScheme().equals( "http" ) || uri.getScheme().equals( "https" );
@@ -29,6 +30,14 @@ public class Links {
         }
 
     }
+
+    public static void checkValid(String link) {
+
+        if ( ! isValid(link)) {
+            throw new IllegalArgumentException( String.format( "Link is not valid: '%s'", link ) );
+        }
+    }
+
 
     public static Link parse( String link ) {
 
