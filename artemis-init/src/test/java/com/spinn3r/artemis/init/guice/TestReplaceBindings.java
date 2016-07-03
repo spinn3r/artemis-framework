f3228bbf6723a537d30991c4d0e3bb8821ae8b10
burton <burton@spinn3r.com>
2016-07-03 15:30:29 -0700

    Fixed test...

---
+++ artemis-init/src/test/java/com/spinn3r/artemis/init/guice/TestReplaceBindings.java
@@ -0,0 +1,59 @@
+package com.spinn3r.artemis.init.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.DependencyAndSource;
+import com.google.inject.spi.ProvisionListener;
+import com.spinn3r.artemis.init.guice.misc.*;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Ignore
+public class TestReplaceBindings {
+
+    @Test
+    public void test1() throws Exception {
+
+        Injector injector = Guice.createInjector( new BillingModule(), new MockBillingModule());
+
+        CreditCardProcessor creditCardProcessor = injector.getInstance(CreditCardProcessor.class);
+
+        Assert.assertEquals(MockCreditCardProcessor.class, creditCardProcessor.getClass());
+
+    }
+
+
+    class BillingModule extends AbstractModule {
+
+        @Override
+        protected void configure() {
+
+            bind(TransactionLog.class ).toInstance( new DefaultTransactionLog() );
+            bind(CreditCardProcessor.class ).to( PaypalCreditCardProcessor.class );
+
+        }
+
+    }
+
+    class MockBillingModule extends AbstractModule {
+
+        @Override
+        protected void configure() {
+
+            bind(CreditCardProcessor.class ).to( MockCreditCardProcessor.class );
+
+        }
+
+    }
+
+
+
+}
