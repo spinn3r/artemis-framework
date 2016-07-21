e96e6f0e1d3de8fbb8d5df5ae4a7c6da4bf59139
burton <burton@spinn3r.com>
2016-07-21 12:04:15 -0700

    Fixed tests...

--- artemis-init/src/main/java/com/spinn3r/artemis/init/AtomicReferenceSupplier.java
+++ artemis-init/src/main/java/com/spinn3r/artemis/init/AtomicReferenceSupplier.java
@@ -5,7 +5,8 @@ import java.util.function.Supplier;
 
 /**
  * A supplier backed by an AtomicReference.  This allows a service to export
- * multiple providers if necessary.
+ * multiple providers if necessary.  Used in cases where Guice gets confused
+ * about Providers and Supplier is a custom non-guice aware class.
  */
 public class AtomicReferenceSupplier<T> implements Supplier<T> {
 
