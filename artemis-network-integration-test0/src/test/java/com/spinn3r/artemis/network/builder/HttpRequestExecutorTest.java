4093d2a75b96fd7636b20bb2804e5be0235b1f35
burton <burton@spinn3r.com>
2016-08-08 18:42:03 -0700

    cleanup to remove httpbin so we have more reliable testing...

--- artemis-network-integration-test0/src/test/java/com/spinn3r/artemis/network/builder/HttpRequestExecutorTest.java
+++ artemis-network-integration-test0/src/test/java/com/spinn3r/artemis/network/builder/HttpRequestExecutorTest.java
@@ -2,17 +2,21 @@ package com.spinn3r.artemis.network.builder;
 
 import com.google.inject.Inject;
 import com.spinn3r.artemis.http.init.DefaultWebserverReferencesService;
+import com.spinn3r.artemis.http.init.WebserverPort;
 import com.spinn3r.artemis.http.init.WebserverService;
+import com.spinn3r.artemis.http.servlets.evaluate.ResponseDescriptor;
 import com.spinn3r.artemis.init.BaseLauncherTest;
+import com.spinn3r.artemis.init.MockHostnameService;
+import com.spinn3r.artemis.init.MockVersionService;
+import com.spinn3r.artemis.metrics.init.MetricsService;
 import com.spinn3r.artemis.network.NetworkException;
 import com.spinn3r.artemis.network.init.DirectNetworkService;
-import com.spinn3r.artemis.time.SyntheticClock;
 import com.spinn3r.artemis.time.init.SyntheticClockService;
+import com.spinn3r.artemis.time.init.UptimeService;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
 
 public class HttpRequestExecutorTest extends BaseLauncherTest {
 
@@ -22,23 +26,34 @@ public class HttpRequestExecutorTest extends BaseLauncherTest {
     @Inject
     HttpRequestBuilder httpRequestBuilder;
 
+    @Inject
+    WebserverPort webserverPort;
+
     @Override
     @Before
     public void setUp() throws Exception {
         super.setUp( SyntheticClockService.class,
-                     DirectNetworkService.class
-//                     DefaultWebserverReferencesService.class,
-//                     WebserverService.class
-        );
+                     MockHostnameService.class,
+                     MockVersionService.class,
+                     UptimeService.class,
+                     MetricsService.class,
+                     DirectNetworkService.class,
+                     DefaultWebserverReferencesService.class,
+                     WebserverService.class);
     }
 
     @Test
     public void testBasicRequestWithoutExceptions() throws Exception {
 
+        String url = new ResponseDescriptor.Builder()
+                             .withStatus(200)
+                             .build()
+                             .toURL("localhost", webserverPort.getPort());
+
         HttpRequestExecutor httpRequestExecutor = httpRequestExecutorFactory.create();
 
         HttpRequest httpRequest
-          = httpRequestExecutor.execute( () -> httpRequestBuilder.get( "https://httpbin.org/status/200" ).execute().connect() );
+          = httpRequestExecutor.execute( () -> httpRequestBuilder.get( url ).execute().connect() );
 
         assertEquals( 200, httpRequest.getResponseCode() );
         assertEquals( 0, httpRequestExecutor.getRetries() );
@@ -48,13 +63,18 @@ public class HttpRequestExecutorTest extends BaseLauncherTest {
     @Test
     public void test404NotFound() throws Exception {
 
+        String url = new ResponseDescriptor.Builder()
+                       .withStatus(404)
+                       .build()
+                       .toURL("localhost", webserverPort.getPort());
+
         HttpRequestExecutor httpRequestExecutor = httpRequestExecutorFactory.create();
 
         NetworkException cause = null;
         HttpRequest httpRequest = null;
 
         try {
-            httpRequest = httpRequestExecutor.execute( () -> httpRequestBuilder.get( "https://httpbin.org/status/404" ).execute().connect() );
+            httpRequest = httpRequestExecutor.execute( () -> httpRequestBuilder.get( url ).execute().connect() );
         } catch ( NetworkException ne ) {
             cause = ne;
         }
@@ -68,13 +88,18 @@ public class HttpRequestExecutorTest extends BaseLauncherTest {
     @Test
     public void test500() throws Exception {
 
+        String url = new ResponseDescriptor.Builder()
+                       .withStatus(500)
+                       .build()
+                       .toURL("localhost", webserverPort.getPort());
+
         HttpRequestExecutor httpRequestExecutor = httpRequestExecutorFactory.create();
 
         NetworkException cause = null;
         HttpRequest httpRequest = null;
 
         try {
-            httpRequest = httpRequestExecutor.execute( () -> httpRequestBuilder.get( "https://httpbin.org/status/500" ).execute().connect() );
+            httpRequest = httpRequestExecutor.execute( () -> httpRequestBuilder.get( url ).execute().connect() );
         } catch ( NetworkException ne ) {
             cause = ne;
         }
