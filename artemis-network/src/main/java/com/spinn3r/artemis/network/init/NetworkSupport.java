c27318c0479bf94a06c2adc75e897d58159fd0d0
burton <burton@spinn3r.com>
2016-09-17 19:11:45 -0700

    This isn't working for now .. not sure why

--- artemis-network/src/main/java/com/spinn3r/artemis/network/init/NetworkSupport.java
+++ artemis-network/src/main/java/com/spinn3r/artemis/network/init/NetworkSupport.java
@@ -1,6 +1,6 @@
 package com.spinn3r.artemis.network.init;
 
-import sun.util.logging.PlatformLogger;
+//import sun.util.logging.PlatformLogger;
 
 /**
  *
@@ -9,7 +9,7 @@ public class NetworkSupport {
 
     public static void disablePlatformLogger() {
         // used so that we don't constantly log cookie information which isn't necessary.
-        PlatformLogger.getLogger("java.net.CookieManager").setLevel(PlatformLogger.Level.OFF);
+        //sun.util.logging.PlatformLogger.getLogger("java.net.CookieManager").setLevel(sun.util.logging.PlatformLogger.Level.OFF);
     }
 
 }
