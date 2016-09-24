e23a85c0782d4560751e0c72dc12cbc2a1883e3a
burton <burton@spinn3r.com>
2016-09-24 10:35:51 -0700

    refactored so that we can take Durations as args...

--- artemis-jcommander/src/main/java/com/spinn3r/artemis/jcommander/converters/optional/OptionalStringConverter.java
+++ artemis-jcommander/src/main/java/com/spinn3r/artemis/jcommander/converters/optional/OptionalStringConverter.java
@@ -1,4 +1,4 @@
-package com.spinn3r.artemis.jcommander.optionals;
+package com.spinn3r.artemis.jcommander.converters.optional;
 
 import com.beust.jcommander.IStringConverter;
 
@@ -7,7 +7,8 @@ import java.util.Optional;
 /**
  *
  */
-public class OptionalStringConverter implements IStringConverter<Optional<String>> {
+public class OptionalStringConverter
+
 
     @Override
     public Optional<String> convert(String value) {
