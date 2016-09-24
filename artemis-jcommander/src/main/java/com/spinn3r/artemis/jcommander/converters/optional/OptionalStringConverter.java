404bff5ed741df655c372a0925848e35d38890e7
burton <burton@spinn3r.com>
2016-09-24 10:48:02 -0700

    Fixed compilation issue.

--- artemis-jcommander/src/main/java/com/spinn3r/artemis/jcommander/converters/optional/OptionalStringConverter.java
+++ artemis-jcommander/src/main/java/com/spinn3r/artemis/jcommander/converters/optional/OptionalStringConverter.java
@@ -7,8 +7,7 @@ import java.util.Optional;
 /**
  *
  */
-public class OptionalStringConverter
-
+public class OptionalStringConverter implements IStringConverter<Optional<String>> {
 
     @Override
     public Optional<String> convert(String value) {
