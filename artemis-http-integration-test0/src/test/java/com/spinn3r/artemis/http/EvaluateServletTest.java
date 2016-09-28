bfe72bb700f4c7a71dcd6c0bc19bfdaa5531e0fb
burton <burton@spinn3r.com>
2016-09-28 12:22:42 -0700

    cleanup of tests...

--- artemis-http-integration-test0/src/test/java/com/spinn3r/artemis/http/EvaluateServletTest.java
+++ artemis-http-integration-test0/src/test/java/com/spinn3r/artemis/http/EvaluateServletTest.java
@@ -1,6 +1,9 @@
 package com.spinn3r.artemis.http;
 
 import com.google.inject.Inject;
+import com.spinn3r.artemis.http.init.DefaultWebserverReferencesService;
+import com.spinn3r.artemis.http.init.WebserverPort;
+import com.spinn3r.artemis.http.init.WebserverService;
 import com.spinn3r.artemis.http.servlets.evaluate.EvaluateServlet;
 import com.spinn3r.artemis.http.servlets.evaluate.ResponseDescriptor;
 import com.spinn3r.artemis.init.BaseLauncherTest;
@@ -8,8 +11,11 @@ import com.spinn3r.artemis.init.MockHostnameService;
 import com.spinn3r.artemis.init.MockVersionService;
 import com.spinn3r.artemis.init.ServiceReferences;
 import com.spinn3r.artemis.logging.init.ConsoleLoggingService;
+import com.spinn3r.artemis.metrics.init.MetricsService;
+import com.spinn3r.artemis.metrics.init.uptime.UptimeMetricsService;
 import com.spinn3r.artemis.network.builder.HttpRequestBuilder;
 import com.spinn3r.artemis.network.init.DirectNetworkService;
+import com.spinn3r.artemis.time.init.UptimeService;
 import org.eclipse.jetty.server.Server;
 import org.junit.After;
 import org.junit.Before;
@@ -21,43 +27,32 @@ import static org.junit.Assert.*;
 
 public class EvaluateServletTest extends BaseLauncherTest {
 
-    private static final int PORT = 8081;
-
     @Inject
     HttpRequestBuilder httpRequestBuilder;
 
-    Server server;
+    @Inject
+    WebserverPort webserverPort;
 
     @Override
     @Before
     public void setUp() throws Exception {
-        super.setUp(DirectNetworkService.class);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-        System.out.printf( "Stopping server..." );
-
-        this.server.stop();
-
+        super.setUp(MockHostnameService.class,
+                    MockVersionService.class,
+                    MetricsService.class,
+                    UptimeService.class,
+                    DirectNetworkService.class,
+                    DefaultWebserverReferencesService.class,
+                    WebserverService.class);
     }
 
     @Test
     public void testEvaluate() throws Exception {
 
-        this.server = new ServerBuilder()
-                        .setPort( PORT )
-                        .addServlet( "/evaluate", new EvaluateServlet())
-                        .build();
-
-        this.server.start();
-
         ResponseDescriptor responseDescriptor
           = new ResponseDescriptor.Builder()
               .build();
 
-        String link = String.format( "http://localhost:%s/evaluate?response=%s", PORT, responseDescriptor.toParam() );
+        String link = String.format( "http://localhost:%s/evaluate?response=%s", webserverPort.getPort(), responseDescriptor.toParam() );
         String content = httpRequestBuilder.get( link ).execute().getContentWithEncoding();
 
     }
