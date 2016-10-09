409a6f4b271ebac7d3ab5a08b5787d5b00b371d5
burton <burton@spinn3r.com>
2016-10-09 09:57:15 -0700

    ok.. basic servlet infrastructure is laid out and working properly.

--- artemis-json/src/main/java/com/spinn3r/artemis/json/TypedErrorMessage.java
+++ artemis-json/src/main/java/com/spinn3r/artemis/json/TypedErrorMessage.java
@@ -9,10 +9,15 @@ public class TypedErrorMessage {
 
     private String message = null;
 
-    private Enum type;
+    private Enum type = null;
 
     public TypedErrorMessage(boolean failed, String message, Enum type) {
         this.failed = failed;
+        this.message = message;
+        this.type = type;
+    }
+
+    public TypedErrorMessage(String message, Enum type) {
         this.message = message;
         this.type = type;
     }
