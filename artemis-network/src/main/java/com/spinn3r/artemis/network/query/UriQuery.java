992a76935766f46ca636b189220c4341840941ba
burton <burton@spinn3r.com>
2016-10-17 12:40:49 -0700

    Ability to split the corpus...

--- artemis-network/src/main/java/com/spinn3r/artemis/network/query/UriQuery.java
+++ artemis-network/src/main/java/com/spinn3r/artemis/network/query/UriQuery.java
@@ -1,12 +1,19 @@
 package com.spinn3r.artemis.network.query;
 
+import java.awt.DisplayMode;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.cert.PKIXRevocationChecker;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A URI query for a fluent API on top of java.net.URI.
  */
 public class UriQuery {
+
+    private static final Pattern DOMAIN_PATTERN = Pattern.compile("[^.]+\\.[^.]+$");
 
     private URI uri;
 
@@ -24,6 +31,22 @@ public class UriQuery {
 
     public String host() {
         return uri.getHost();
+    }
+
+    public Optional<String> domain() {
+
+        String host = host();
+
+        if (host == null)
+            return Optional.empty();
+
+        Matcher matcher = DOMAIN_PATTERN.matcher(host);
+        if (matcher.find()) {
+            return Optional.of(matcher.group());
+        }
+
+        return Optional.empty();
+
     }
 
     public static UriQuery uri( String value ) {
