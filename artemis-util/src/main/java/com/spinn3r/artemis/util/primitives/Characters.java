4c1cdb352b710e3973b7e9aa571ef6a885efb798
burton <burton@spinn3r.com>
2016-11-26 18:16:24 -0800

    password generator

---
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/primitives/Characters.java
@@ -0,0 +1,22 @@
+package com.spinn3r.artemis.util.primitives;
+
+import com.google.common.collect.ImmutableList;
+import com.spinn3r.artemis.guava.GuavaCollectors;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class Characters {
+
+    public static ImmutableList<Character> range(char start, char end) {
+
+        return Integers.range(start, end)
+                       .stream()
+                       .map( current -> (char)current.intValue())
+                       .collect(GuavaCollectors.toImmutableList());
+
+    }
+
+}
