8b8bd6544bf0034f5ea994a6587a9d530edf86ca
burton <burton@spinn3r.com>
2016-12-01 10:34:46 -0800

    ability to use static but seemingly random IPs...

--- artemis-util/src/main/java/com/spinn3r/artemis/util/misc/ImmutableCollectionUtils.java
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/misc/ImmutableCollectionUtils.java
@@ -1,10 +1,12 @@
 package com.spinn3r.artemis.util.misc;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -14,10 +16,16 @@ public class ImmutableCollectionUtils {
     /**
      * Shuffle the given list, then return it.
      */
-    public static <T> List<T> shuffle(List<T> list ) {
+    public static <T> ImmutableList<T> shuffle(List<T> list) {
         ArrayList<T> tmp = Lists.newArrayList(list);
-        Collections.shuffle(tmp );
-        return tmp;
+        Collections.shuffle(tmp);
+        return ImmutableList.copyOf(tmp);
+    }
+
+    public static <T> ImmutableList<T> shuffle(List<T> list, Random random) {
+        ArrayList<T> tmp = Lists.newArrayList(list);
+        Collections.shuffle(tmp, random);
+        return ImmutableList.copyOf(tmp);
     }
 
 }
