59d36ba16c06379381a590d460a25dadc3712630
burton <burton@spinn3r.com>
2016-12-09 16:42:35 -0800

    Use year month date format so its easier to work with from json land

--- artemis-datetime/src/main/java/com/spinn3r/artemis/datetime/LocalDateTimes.java
+++ artemis-datetime/src/main/java/com/spinn3r/artemis/datetime/LocalDateTimes.java
@@ -1,10 +1,7 @@
 package com.spinn3r.artemis.datetime;
 
-import com.spinn3r.artemis.time.TimeZones;
-
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Date;
@@ -15,7 +12,7 @@ import java.util.Optional;
  */
 public class LocalDateTimes {
 
-    public static Date toDate( LocalDateTime localDateTime ) {
+    public static Date toDate(LocalDateTime localDateTime) {
         return Date.from( localDateTime.toInstant( ZoneOffset.UTC ) );
     }
 
