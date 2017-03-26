db98a358fa51b159f76e6389fe069e0dcd58fcfc
burton <burton@spinn3r.com>
2017-03-26 16:01:06 -0700

    everything should work now..

--- artemis-corpus-test-framework/src/main/java/com/spinn3r/artemis/corpus/test/CorporaAsserter.java
+++ artemis-corpus-test-framework/src/main/java/com/spinn3r/artemis/corpus/test/CorporaAsserter.java
@@ -97,15 +97,11 @@ public class CorporaAsserter {
 
             String expected = corporaCache.read( key );
 
-            if ( expected == null ) {
-                throw new IOException( "Could not read from cache: " + expected );
-            }
-
             String path = corporaCache.computePath( key );
 
-            if ( ! Objects.equals( expected, actual ) ) {
+            String msg = computeFailureMsg( path, expected, actual );
 
-                String msg = computeFailureMsg( path, expected, actual );
+            if ( ! Objects.equals( expected, actual ) ) {
 
                 throw new CorporaComparisonFailure( msg, expected, actual );
 
@@ -125,7 +121,11 @@ public class CorporaAsserter {
         buff.append( String.format( "path: %s\n", path ) );
         buff.append( "=== DIFF START\n\n" );
 
-        buff.append( DiffGenerator.diff( expected, actual ) );
+        if(expected != null) {
+            buff.append(DiffGenerator.diff(expected, actual));
+        } else {
+            buff.append("`expected` was null\n ");
+        }
 
         buff.append( "\n\n" );
         buff.append( "=== DIFF END\n\n" );
