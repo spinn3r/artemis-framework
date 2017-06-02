f2eab016011bf46c1ab5952de6fc8c5f78d129d5
Miguel <miguel@spinn3r.com>
2017-06-02 22:25:46 +0200

    Finding the most common element in a list

--- artemis-util/src/main/java/com/spinn3r/artemis/util/misc/CollectionUtils.java
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/misc/CollectionUtils.java
@@ -1,11 +1,6 @@
 package com.spinn3r.artemis.util.misc;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 import com.google.common.collect.ImmutableList;
@@ -278,6 +273,26 @@ public class CollectionUtils {
 
         return ImmutableList.copyOf(result);
 
+    }
+
+
+    public static <T> T mostCommon(List<T> list) {
+
+        Map<T, Integer> map = new HashMap<>();
+
+        for (T t : list) {
+            Integer val = map.get(t);
+            map.put(t, val == null ? 1 : val + 1);
+        }
+
+        Map.Entry<T, Integer> max = null;
+
+        for (Map.Entry<T, Integer> e : map.entrySet()) {
+            if (max == null || e.getValue() > max.getValue())
+                max = e;
+        }
+
+        return max.getKey();
     }
 
 
