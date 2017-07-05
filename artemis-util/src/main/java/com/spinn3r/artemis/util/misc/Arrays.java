77896a5e86c52f4f35f6caf15f3eda47c6681242
Miguel <miguel@spinn3r.com>
2017-07-06 00:57:53 +0200

    Utility class

---
+++ artemis-util/src/main/java/com/spinn3r/artemis/util/misc/Arrays.java
@@ -0,0 +1,12 @@
+package com.spinn3r.artemis.util.misc;
+
+public final class Arrays {
+
+    private Arrays() {
+    }
+
+    public static <T> T[] asArray(T ... array){
+        return array;
+    }
+
+}
